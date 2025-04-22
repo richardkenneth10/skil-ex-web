47d5683dafdd5691b0081eaba01b740351ed5ced
Richard <79406473+richardkenneth10@users.noreply.github.com>
2025-04-22 01:24:00 +0100

    try fix failing images

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,16 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "localhost",
       },
+      {
+        protocol: process.env.PUBLIC_API_BASE_URL?.substring(
+          0,
+          process.env.PUBLIC_API_BASE_URL?.indexOf("://")
+        ) as "https" | "http" | undefined,
+        hostname:
+          process.env.PUBLIC_API_BASE_URL?.substring(
+            process.env.PUBLIC_API_BASE_URL?.indexOf("://")
+          ) ?? "",
+      },
     ],
   },
   env: {
